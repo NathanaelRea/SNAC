a6c91277d2152f4a988ad5efaa0a49c100550022
Nathanael Rea <nrea@eng.ucsd.edu>
2020-12-29 18:07:20 -0800

    Dev (#1)
    * moved functions to structures header for clarity
    
    * added element local force
    
    * refactored input pase
    
    * added nodal point loads and better printing

--- README.md
+++ README.md
@@ -2,13 +2,37 @@
 
 Structural Numeric Analysis in C++
 
-A structural analysis solver nobody needed in C++. Just a proof of concept for now, can only add point and distributed loads on elements, and only outputs the solved u vector.
+A very simple structural analysis solver nobody needed, and especially not in C++. Just a proof of concept for now.
+
+Lmitations:
+2D Linear Small Deformations
+Only point and distributed loads on elements
+Only outputs the solved u vector and local forces
 
 ## Compile with correct libraries
 
 ```
 $ g++ -std=c++17 -O2 -larmadillo -llapack -lblas
 ```
+
+## Input Syntax
+```
+POINTS
+<X> <Y>
+
+ELEMENTS
+<P#1> <P#2> <E> <I> <A>
+
+BOUNDS
+<P#> <DIR> <DISP>
+
+LOADING
+ELEMENT <E#> DISTRIBUTED <W1> <W2>
+ELEMENT <E#> POINT <LOAD> <DIST>
+POINT <P#> <DIR> <LOAD>
+```
+Points and Elements are 0 indexed
+
 
 ## Example input file
 
@@ -39,17 +63,25 @@ ELEMENT 1 POINT -10 96
 Output:
 
 ```
-Solved u
-        0
-        0
-        0
-   0.2095
-  -0.1587
-  -0.0023
-   0.2095
-  -0.0020
-   0.0034
-   0.5350
-        0
-   0.0034
+Point Displacement (X,Y,R)
+Point 0
+(0.000000, 0.000000, 0.000000)
+Point 1
+(0.209509, -0.158741, -0.002346)
+Point 2
+(0.209509, -0.002023, 0.003391)
+Point 3
+(0.535009, 0.000000, 0.003391)
+
+Local Element End Forces (A,V,M)
+Element 0
+(3.111614, 17.333710, 966.832579)
+(-3.111614, -2.333710, 213.212670)
+Element 1
+(0.000000, 3.889517, -213.212670)
+(-0.000000, 6.110483, -0.000000)
+Element 2
+(6.110483, 0.000000, 0.000000)
+(-6.110483, -0.000000, 0.000000)
+
 ```
--- SNAC.cpp
+++ SNAC.cpp
@@ -12,146 +12,141 @@
 using namespace std;
 using namespace arma;
 
-void parseInput(string filename, Structure *input);
-Mat<double>createLocalStiffness(Element *e);
-size_t split(const string &txt, vector<string> &strs, char ch);
+void parseFile(string filename, Structure *s);
+void parseRawInput(RawInput *input, Structure *s);
 int dirConvert (string dir);
-
-Mat<double> constrainK(Mat<double> K, Structure *system) {
-	// Modify Force Vector and Stiffness Matrix for given boundary condition
-	Mat<double> Kcopy = K;
-	int nBounds = (int)system->bounds.size();
-	int nDof = (int)system->points.size()*3;
-	for (int iBound = 0; iBound < nBounds; iBound++) {
-		int dof = system->bounds[iBound].dof;
-		for (int i = 0; i < nDof; i++) {
-			system->globalLoad(i) -= Kcopy(dof,i)*system->bounds[iBound].disp;
-			Kcopy(i,dof) = 0;
-			Kcopy(dof,i) = 0;
-		}
-		system->globalLoad(dof) = system->bounds[iBound].disp;
-		Kcopy(dof,dof) = 1;
-	}
-	return Kcopy;
-}
+size_t split(const string &txt, vector<string> &strs, char ch);
 
 int main(int argc, char *argv[]) {
 	if (argc != 2)
 		throw runtime_error("Please specify input file");
-	Structure system;
-	parseInput(argv[1], &system);
-	int nEle = (int)system.elements.size();
-	int nDof = (int)system.points.size()*3;
-	Mat<double> K = zeros(nDof,nDof);
 	
-	for (int e = 0; e < nEle; e++) {
-		Mat<double> KBar;
-		KBar = createLocalStiffness(&system.elements[e]);
-		for (int localI = 0; localI < 6; localI++) {
-			for (int localJ = 0; localJ < 6; localJ++) {
-				int globalI = system.elements[e].IDArray[localI];
-				int globalJ = system.elements[e].IDArray[localJ];
-				K(globalI, globalJ) += KBar(localI, localJ);
-			}
-		}
-	}
-	Mat<double> Kbounded = constrainK(K, &system);
-	Mat<double> u = solve(Kbounded, system.globalLoad);
-	cout << "Solved u\n" << u << endl;
-	/* TODO
-	 * Calculate Element forces from nodal disp & Solved Force Vector
-	 */
+	Structure system;
+	parseFile(argv[1], &system);
+	
+	system.assembleK();
+	system.solve();
+	
+	system.printNodeDisp();
+	system.printEleForce();
+	
 	return 0;
 }
 
-void parseInput(string filename, Structure *input) {
-	int nodeNum = 0;
-	
-	vector<string> data;
-	int inputBlock = -1;
-	string line;
-	
+void parseFile(string filename, Structure *s) {
 	ifstream myfile (filename);
-	if (myfile.is_open()) {
-		while ( getline (myfile,line) ) {
-			if (line == "POINTS") {
-				inputBlock = 0;
-				continue;
-			} else if (line == "ELEMENTS") {
-				inputBlock = 1;
-				input->globalLoad.zeros((int)input->points.size()*3);
-				continue;
-			} else if (line == "BOUNDS") {
-				inputBlock = 2;
-				continue;
-			} else if (line == "LOADING") {
-				inputBlock = 3;
-				continue;
-			} else if (line == "") {
-				inputBlock = -1;
-				continue;
-			};
-			split(line, data, ' ');
-			switch(inputBlock) {
-				case 0: {
-					// Points
-					double x = atof(data[0].c_str());
-					double y = atof(data[1].c_str());
-					Point tmpPoint {nodeNum, x, y};
-					input->points.push_back(tmpPoint);
-					nodeNum++;
-					break;
-				}
-				case 1: {
-					// Elements
-					int p1 = stoi(data[0]);
-					int p2 = stoi(data[1]);
-					double E = atof(data[2].c_str());
-					double I = atof(data[3].c_str());
-					double A = atof(data[4].c_str());
-					Element tmpElement {&input->points[p1], &input->points[p2], E, I, A};
-					input->elements.push_back(tmpElement);
-					break;
-				}
-				case 2: {
-					// Bounds
-					int p = stoi(data[0]);
-					int dof = input->points[p].n*3 + dirConvert(data[1]);
-					double disp = atof(data[2].c_str());
-					Bound tmpBound {dof, disp};
-					input->bounds.push_back(tmpBound);
-					break;
-				}
-				case 3: {
-					// Loading
-					vector<double> tmpLoad;
-					if (data[0] == "ELEMENT") {
-						Element *tmpElement = &input->elements[stoi(data[1])];
-						double L = tmpElement->L;
-						// tmpLoad = {F1, M1, F2, M2}
-						if (data[2] == "DISTRIBUTED") {
-							double w1 = atof(data[3].c_str());
-							double w2 = atof(data[4].c_str());
-							tmpLoad = { (7*w1+3*w2)*L/20, (w1/2+w2/3)*pow(L,2)/10, \
-										(3*w1+7*w2)*L/20,-(w1/3+w2/2)*pow(L,2)/10};
-						} else if (data[2] == "POINT") {
-							double P = atof(data[3].c_str());
-							double a = atof(data[4].c_str());
-							double b = L-a;
-							tmpLoad = { P*pow(b,2)*(3*a+b)/pow(L,3), P*a*pow(b,2)/pow(L,2), \
-										P*pow(a,2)*(3*b+a)/pow(L,3),-P*b*pow(a,2)/pow(L,2)};
-						} else {
-							throw runtime_error("ELEMENT type loading must be either 'DISTRIBUTED' or 'POINT'");
-						}
-						// Add Local Element Vector to Global Force Vector
-						input->addElementLoad(tmpElement, tmpLoad);
-					} else {
-						throw runtime_error("Can only handle Loading of type 'ELEMENT'");
-					}
-				}
-			}
+	if (!myfile.is_open())
+		throw runtime_error("Could not open file.");
+	
+	string line;
+	vector<string> data;
+	RawInput input;
+	int inputBlock = -1;
+	
+	while (getline (myfile,line)) {
+		if (line == "POINTS") {
+			inputBlock = 0;
+			continue;
+		} else if (line == "ELEMENTS") {
+			inputBlock = 1;
+			continue;
+		} else if (line == "BOUNDS") {
+			inputBlock = 2;
+			continue;
+		} else if (line == "LOADING") {
+			inputBlock = 3;
+			continue;
+		} else if (line == "") {
+			inputBlock = -1;
+			continue;
+		};
+		split(line, data, ' ');
+		switch(inputBlock) {
+			case 0:
+				input.points.push_back(data);
+				break;
+			case 1:
+				input.elements.push_back(data);
+				break;
+			case 2:
+				input.bounds.push_back(data);
+				break;
+			case 3:
+				input.loads.push_back(data);
+				break;
 		}
-		myfile.close();
+	}
+	myfile.close();
+	
+	parseRawInput(&input, s);
+}
+	
+void parseRawInput(RawInput *input, Structure *s) {
+	int nPoint = (int)input->points.size();
+	int nDof = nPoint*3;
+	int nEle = (int)input->elements.size();
+	int nBound = (int)input->bounds.size();
+	int nLoad = (int)input->loads.size();
+	
+	s->setSize(nDof);
+	vector<string> data;
+	
+	for (int i = 0; i < nPoint; i++) {
+		data = input->points[i];
+		double x = atof(data[0].c_str());
+		double y = atof(data[1].c_str());
+		Point tmpPoint {i, x, y};
+		s->points.push_back(tmpPoint);
+	}
+	for (int i = 0; i < nEle; i++) {
+		data = input->elements[i];
+		int p1 = stoi(data[0]);
+		int p2 = stoi(data[1]);
+		double E = atof(data[2].c_str());
+		double I = atof(data[3].c_str());
+		double A = atof(data[4].c_str());
+		Element tmpElement {&s->points[p1], &s->points[p2], E, I, A};
+		s->elements.push_back(tmpElement);
+	}
+	for (int i = 0; i < nBound; i++) {
+		data = input->bounds[i];
+		int p = stoi(data[0]);
+		int dof = s->points[p].n*3 + dirConvert(data[1]);
+		double disp = atof(data[2].c_str());
+		Bound tmpBound {dof, disp};
+		s->bounds.push_back(tmpBound);
+	}
+	for (int i = 0; i < nLoad; i++) {
+		data = input->loads[i];
+		vector<double> tmpLoad;
+		if (data[0] == "POINT") {
+			int p = stoi(data[1]);
+			int dof = s->points[p].n*3 + dirConvert(data[2]);
+			double load = atof(data[3].c_str());
+			s->addPointLoad(dof, load);
+		} else if (data[0] == "ELEMENT") {
+			Element *tmpElement = &s->elements[stoi(data[1])];
+			double L = tmpElement->L;
+			// tmpLoad = {F1, M1, F2, M2}
+			if (data[2] == "DISTRIBUTED") {
+				double w1 = atof(data[3].c_str());
+				double w2 = atof(data[4].c_str());
+				tmpLoad = { (7*w1+3*w2)*L/20, (w1/2+w2/3)*pow(L,2)/10,
+					(3*w1+7*w2)*L/20,-(w1/3+w2/2)*pow(L,2)/10};
+			} else if (data[2] == "POINT") {
+				double P = atof(data[3].c_str());
+				double a = atof(data[4].c_str());
+				double b = L-a;
+				tmpLoad = { P*pow(b,2)*(3*a+b)/pow(L,3), P*a*pow(b,2)/pow(L,2),
+					P*pow(a,2)*(3*b+a)/pow(L,3),-P*b*pow(a,2)/pow(L,2)};
+			} else {
+				throw runtime_error("ELEMENT type loading must be either 'DISTRIBUTED' or 'POINT'");
+			}
+			// Add Local Element Vector to structure
+			s->addElementLoad(tmpElement, tmpLoad);
+		} else {
+			throw runtime_error("Can only handle Loading of type 'ELEMENT' or 'POINT'");
+		}
 	}
 }
 
@@ -170,49 +165,21 @@ int dirConvert (string dir) {
 	return dof;
 }
 
-Mat<double> createLocalStiffness(Element *e) {
-	// Dereference into local variables for clarity
-	double E = e->E;
-	double I = e->I;
-	double A = e->A;
-	double L = e->L;
-	// 1D Local Stiffness Matrix
-	Mat<double>localK = {	{E*A/L,       0,       0},
-							{    0, 4*E*I/L, 2*E*I/L},
-							{    0, 2*E*I/L, 4*E*I/L}};
-	// Rigid Body Matrix
-	Mat<double>RB = {	{-1,   0, 0, 1,    0, 0},
-						{ 0, 1/L, 1, 0, -1/L, 0},
-						{ 0, 1/L, 0, 0, -1/L, 1}};
-	// Rotational Matrix
-    double c = cos(e->theta);
-    double s = sin(e->theta);
-	Mat<double>ROT = {	{ c, s, 0, 0, 0, 0},
-						{-s, c, 0, 0, 0, 0},
-						{ 0, 0, 1, 0, 0, 0},
-						{ 0, 0, 0, c, s, 0},
-						{ 0, 0, 0,-s, c, 0},
-						{ 0, 0, 0, 0, 0, 1}};
-	// Combine into Global Domain
-	Mat<double> kBar = ROT.t() * RB.t() * localK * RB * ROT;
-	return kBar;
-}
-
 size_t split(const string &txt, vector<string> &strs, char ch) {
 	// from https://stackoverflow.com/questions/5888022/split-string-by-single-spaces
 	// used to parse input file lines
-    size_t pos = txt.find( ch );
-    size_t initialPos = 0;
-    strs.clear();
-
-    // Decompose statement
-    while( pos != std::string::npos ) {
+	size_t pos = txt.find( ch );
+	size_t initialPos = 0;
+	strs.clear();
+	
+	// Decompose statement
+	while( pos != std::string::npos ) {
         strs.push_back( txt.substr( initialPos, pos - initialPos ) );
         initialPos = pos + 1;
         pos = txt.find( ch, initialPos );
-    }
-
-    // Add the last one
-    strs.push_back( txt.substr( initialPos, min( pos, txt.size() ) - initialPos + 1 ) );
-    return strs.size();
+	}
+	
+	// Add the last one
+	strs.push_back( txt.substr( initialPos, min( pos, txt.size() ) - initialPos + 1 ) );
+	return strs.size();
 }
